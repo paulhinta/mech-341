d507785e819f7651d8333ad7427ee40481b348a5
paulhinta <paul_hinta@outlook.com>
2022-03-12 15:28:37 -0500

    q1

---
+++ enthalpy.py
@@ -0,0 +1,14 @@
+import sympy as sp
+
+def enthalpy(input: dict, T:int=298.15):
+    x = sp.Symbol("x")
+    
+    def f(x):
+        return input["a1"]*x**-2 + input["a2"]*x**-1 + input["a3"] + input["a4"]*x + input["a5"]*x**2 + input["a6"]*x**3 + input["a7"]*x**4
+
+    int_f = sp.integrate(f(x),x)
+    lower_f = float(int_f.subs(x, 298.15))
+    b1 = input["b1"]
+
+    print("finished with " + input["Specie"])
+    return float(int_f.subs(x, T) - lower_f + 8.314*b1)
--- task1.py
+++ task1.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv      #loads secret environment variable (mongo pa
 import os                           #fetches password from environment
 import sympy as sp
 import numpy as np
+from enthalpy import enthalpy       #function to calculate enthalpy
 
 #from calc_hf import calc            #side function to calculate hf
 
@@ -18,43 +19,20 @@ ca = certifi.where()                #mongo connection certificate
 client = pymongo.MongoClient(connect, tlsCAFile=ca)
 db = client['inputs']               #database that we are using
 collection=db['selected-data']      #connection that we are using
-cursor = collection.find({"Specie": "H2O"})  #get H2
 
 #THERMO STUFF STARTS HERE
 
-#declare variables of integration
-x = sp.Symbol("x")
-T = sp.Symbol("T")
+#declare variables of math
+p = sp.Symbol("p")
 
-#first function for cp; water vapour < 1000K
-def f(x):
-    return cursor[0]["a1"]*x**-2 + cursor[0]["a2"]*x**-1 + cursor[0]["a3"] + cursor[0]["a4"]*x + cursor[0]["a5"]*x**2 + cursor[0]["a6"]*x**3 + cursor[0]["a7"]*x**4
+h2o = collection.find_one({"index":11})
+o2 = collection.find_one({"index":13})
+n2 = collection.find_one({"index":15})
 
-#compute upper & lower limits of integration
-int_f = sp.integrate(f(x),x)
-lower_f = float(int_f.subs(x, 298))
-upper_f = int_f.subs(x, T)
-cp_Tf = upper_f - lower_f +8.314*cursor[0]["b1"]       ##symbolic representation of cp as f(T)
-print(cp_Tf)
-
-#second function for cp; water vapour > 1000K
-def g(x):
-    return cursor[1]["a1"]*x**-2 + cursor[1]["a2"]*x**-1 + cursor[1]["a3"] + cursor[1]["a4"]*x + cursor[1]["a5"]*x**2 + cursor[1]["a6"]*x**3 + cursor[1]["a7"]*x**4
-
-#compute upper & lower limits of integration
-int_g = sp.integrate(g(x),x)
-lower_g = float(int_g.subs(x, 298))
-upper_g = int_g.subs(x, T)
-cp_Tg = upper_g - lower_g +8.314*cursor[1]["b1"]       ##symbolic representation of cp as f(T)
-print(cp_Tg)
-
-
-
+#critical phi
+p_t = p*enthalpy(h2o, 1000) + 3.76*enthalpy(n2, 1000) + (2-p)*enthalpy(o2, 1000)
+p_base = sp.solve(p_t, p)
+print(p_base)
 
 
 er = 0 #equivalence ratio
-# while er <= 2:
-#     print(er)
-#     #print(calc({},{}))
-#     x.append(er)
-#     er += 0.02
