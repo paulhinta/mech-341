8cab868e9fa7789b7f4b3d29c7a9704ca4327ade
paulhinta <paul_hinta@outlook.com>
2022-03-13 17:45:26 -0400

    finished question 1, set up an analysis.py function + update db functions

--- enthalpy.py
+++ enthalpy.py
@@ -1,4 +1,5 @@
 import sympy as sp
+from sympy import lambdify, nsolve
 
 def enthalpy(input:dict, T:int=298.15):
     x = sp.Symbol("x")
@@ -7,9 +8,35 @@ def enthalpy(input:dict, T:int=298.15):
         return input["a1"]*x**-2 + input["a2"]*x**-1 + input["a3"] + input["a4"]*x + input["a5"]*x**2 + input["a6"]*x**3 + input["a7"]*x**4
 
     #print(f(x))
-
     int_f = sp.integrate(f(x),x)
-    lower_f = float(int_f.subs(x, 298.15))
     b1 = input["b1"]
 
-    return float(int_f.subs(x, T) - lower_f + 8.314*b1)
+    return 8.314*float(int_f.subs(x, T) + b1)
+
+def enthalpy_function(input:dict, T=298.15):
+    x = sp.Symbol("x")
+    
+    def f(x):
+        return input["a1"]*x**-2 + input["a2"]*x**-1 + input["a3"] + input["a4"]*x + input["a5"]*x**2 + input["a6"]*x**3 + input["a7"]*x**4
+
+    #print(f(x))
+    int_f = sp.integrate(f(x),x)
+    b1 = input["b1"]
+
+    return 8.314*(int_f.subs(x, T) + b1)
+
+def aft(water:dict, nitrogen:dict, extra:dict,  er:float=1.0):
+    er_2 = er
+    if er >=1:
+        er_2 = 1
+    
+    T = sp.Symbol("T")
+
+    def f(T):
+        return 2*er_2*enthalpy_function(water, T) + 3.76*enthalpy_function(nitrogen, T) + abs(2*(er - 1))*enthalpy_function(extra, T)
+    
+    eq = f(T)
+
+    T_aft = nsolve(eq, 1)
+
+    return T_aft
--- task1.py
+++ task1.py
@@ -4,7 +4,9 @@ from dotenv import load_dotenv      #loads secret environment variable (mongo pa
 import os                           #fetches password from environment
 import sympy as sp
 import numpy as np
-from enthalpy import enthalpy       #function to calculate enthalpy
+from enthalpy import enthalpy, aft       #function to calculate enthalpy, adiabatic flame temp
+
+#FILLS UP THE MONGODB WITH data for question 1, it can easily be accessed by running analysis.py
 
 #from calc_hf import calc            #side function to calculate hf
 
@@ -25,19 +27,55 @@ collection=db['selected-data']      #connection that we are using
 #declare variables of math
 p = sp.Symbol("p")
 
-h2o_1000 = collection.find_one({"index":11})
-o2_1000 = collection.find_one({"index":13})
-n2_1000 = collection.find_one({"index":15})
+#fetch the species data from the db
+h2o_1000 = collection.find_one({"index":10})
+o2_1000 = collection.find_one({"index":12})
+n2_1000 = collection.find_one({"index":14})
 
-al2o3_s = collection.find_one({"index":2})
-al2o3_l = collection.find_one({"index":3})
-
-print(al2o3_s)
-print(al2o3_l)
-
-print(enthalpy(al2o3_s, 2327))
-print(enthalpy(al2o3_l, 2327))
+#critical point phi:
+g = 2*p*enthalpy(h2o_1000, 1000) + 3.76*enthalpy(n2_1000, 1000) + 2*(1-p)*enthalpy(o2_1000, 1000)
+p_crit = sp.solve(g, p)[0]
 
 ers = []
+afts = []
 
 er = 0 #equivalence ratio
+
+#fuel rich species
+fr_h2o = collection.find_one({"index":11})   #h2o
+fr_n2 = collection.find_one({"index":15})   #n2
+fr_h2 = collection.find_one({"index":8})    #h2
+
+#fuel lean case 1, er >= p_crit
+f1_h2o = fr_h2o                             #h2o, same conditions as fuel rich
+f1_n2 = fr_n2                               #n2, same conditinos as fuel rich
+f1_o2 = collection.find_one({"index":13})                             #o2 instead of h2, use value at 1000K
+
+#fuel lean case 2, er < p_crit -> use values from before (species 10, 12 14)
+
+T_aft = 0       #adiabatic flame temp (dynamic variable)
+
+while True:
+    if er > 2:
+        break
+
+    #fuel rich
+    if er >= 1:
+        T_aft = aft(fr_h2o, fr_n2, fr_h2, er)
+    
+    #fuel lean, er > p_crit
+    elif er >= p_crit:
+        T_aft = aft(f1_h2o, f1_n2, f1_o2, er)
+
+    #fuel lean, er < p_crit
+    else:
+        T_aft = aft(h2o_1000, n2_1000, o2_1000, er)
+
+    afts.append(float(T_aft))
+    ers.append(er)
+    er += 0.02
+
+#connect to the database and load data into the question 1 collection
+collection = db['q1']
+collection.update_one({"index":0}, {"$set": {"Axis":"X", "Data":ers}}, upsert=True)
+collection.update_one({"index":1}, {"$set": {"Axis":"Y", "Data":afts}}, upsert=True)
