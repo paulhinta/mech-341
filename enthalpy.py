8c886ece6812cc019993642121fa20fb9e27057a
paulhinta <paul_hinta@outlook.com>
2022-03-13 20:34:06 -0400

    added comments to task 1 functions

--- enthalpy.py
+++ enthalpy.py
@@ -1,18 +1,26 @@
 import sympy as sp
 from sympy import lambdify, nsolve
 
+#Python functions to calculate the enthalpy & adiabatic flame temp of a species based on the a_n & b_n constants
+
+#calculate enthalpy (absolute value) using sympy
 def enthalpy(input:dict, T:int=298.15):
     x = sp.Symbol("x")
     
+    #define the polynomial
     def f(x):
         return input["a1"]*x**-2 + input["a2"]*x**-1 + input["a3"] + input["a4"]*x + input["a5"]*x**2 + input["a6"]*x**3 + input["a7"]*x**4
 
     #print(f(x))
+    #integrate the polynomial
     int_f = sp.integrate(f(x),x)
     b1 = input["b1"]
 
+    #evaluate the polynomial
     return 8.314*float(int_f.subs(x, T) + b1)
 
+#Symbolically represent the enthalpy as a function of T
+#this does the same as the above function, but returns a function of T rather than evaluating the definite integral
 def enthalpy_function(input:dict, T=298.15):
     x = sp.Symbol("x")
     
@@ -25,6 +33,8 @@ def enthalpy_function(input:dict, T=298.15):
 
     return 8.314*(int_f.subs(x, T) + b1)
 
+#this function uses the enthalpy as a function of T (above) to evaluate the AFT for question 1
+#each instance of an aft will have water, nitrogen, and an excess gas (h2 or o2)
 def aft(water:dict, nitrogen:dict, extra:dict,  er:float=1.0):
     er_2 = er
     if er >=1:
@@ -32,11 +42,14 @@ def aft(water:dict, nitrogen:dict, extra:dict,  er:float=1.0):
     
     T = sp.Symbol("T")
 
+    #sub the enthalpy as a function of T (complex function)
     def f(T):
         return 2*er_2*enthalpy_function(water, T) + 3.76*enthalpy_function(nitrogen, T) + abs(2*(er - 1))*enthalpy_function(extra, T)
     
     eq = f(T)
 
+    #numerically solve the function of T
+    #this is done numerically because it's a nonlinear function, sympy can only symbolically solve linear functions
     T_aft = nsolve(eq, 1)
 
     return T_aft
