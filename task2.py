73cd00974a0e1f400dba9b350ac9d4871e725d6e
paulhinta <paul_hinta@outlook.com>
2022-03-13 20:30:03 -0400

    started q2

--- task2.py
+++ task2.py
@@ -2,7 +2,6 @@ import pymongo                      #python + mongo
 import certifi                      #to allow user certification when connecting to Mongo
 from dotenv import load_dotenv      #loads secret environment variable (mongo password)
 import os                           #fetches password from environment
-import matplotlib.pyplot as plt
 
 #FILLS UP THE MONGODB WITH data for question 1, it can easily be accessed by running analysis.py
 
@@ -18,16 +17,8 @@ connect = "mongodb+srv://paulhinta:" + pw + "@cluster0.p3kas.mongodb.net/inputs?
 ca = certifi.where()                #mongo connection certificate
 client = pymongo.MongoClient(connect, tlsCAFile=ca)
 db = client['inputs']               #database that we are using
+collection=db['selected-data']      #connection that we are using
 
 '''
-TASK 1 
+THERMO STUFF STARTS HERE
 '''
-collection=db['q1']                 #connection that we are using
-
-#Fetch q1 data from the database
-x_axis = collection.find_one({"Axis":"X"})["Data"]
-y_axis = collection.find_one({"Axis":"Y"})["Data"]
-
-#Plot the question 1 data
-plt.plot(x_axis, y_axis)
-plt.show()
